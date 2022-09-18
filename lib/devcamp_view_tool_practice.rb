99125b54d769e0345743ea6d6088c48ab8e2f315
Amr Zayet <amrz.edu@gmail.com>
2022-09-18 23:54:16 +0300

    initial commit: gem generated

---
+++ lib/devcamp_view_tool_practice.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "devcamp_view_tool_practice/version"
+
+module DevcampViewToolPractice
+  class Error < StandardError; end
+  # Your code goes here...
+end
